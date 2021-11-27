60ffa210ee1f71bf43a794f56e74d411a6a500dd
Paolo Porcellato <paolo.porcellato@gmail.com>
2021-11-27 15:28:41 +0100

    primo coomit

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+let btnMenuClose = document.getElementById("btn_menu_close");
+let btnMenuOpen = document.getElementById("btn_menu_open");
+btnMenuClose.addEventListener("click",() => {
+    let menu = document.getElementById("menu");
+    menu.classList.add("menu--close");
+    menu.classList.remove("menu--open");
+});
+btnMenuOpen.addEventListener("click",() => {
+    let menu = document.getElementById("menu");
+    menu.classList.add("menu--open");
+    menu.classList.remove("menu--close");
+});
