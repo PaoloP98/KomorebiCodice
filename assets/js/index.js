f272a65b3e7aee7ec090e1c2e4a648db5530dc61
Paolo Porcellato <paolo.porcellato@gmail.com>
2021-11-28 09:37:26 +0100

    fix alberelli

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,22 @@
 let btnMenuClose = document.getElementById("btn_menu_close");
 let btnMenuOpen = document.getElementById("btn_menu_open");
-btnMenuClose.addEventListener("click",() => {
-    let menu = document.getElementById("menu");
-    menu.classList.add("menu--close");
-    menu.classList.remove("menu--open");
+btnMenuClose.addEventListener("click", () => {
+  let menu = document.getElementById("menu");
+  menu.classList.add("menu--close");
+  menu.classList.remove("menu--open");
 });
-btnMenuOpen.addEventListener("click",() => {
-    let menu = document.getElementById("menu");
-    menu.classList.add("menu--open");
-    menu.classList.remove("menu--close");
+btnMenuOpen.addEventListener("click", () => {
+  let menu = document.getElementById("menu");
+  menu.classList.add("menu--open");
+  menu.classList.remove("menu--close");
 });
 
-$(document).scroll(function() {
-    if ($('#alberelli').offset().top + $('#alberelli').height() >= $('footer').offset().top - 10)
-      $('#alberelli').css({
-        'position': 'absolute',
-        'bottom': 0,
-      });
-  
-    // if ($(document).scrollTop() + $('#alberelli').height() < $('.footer').offset().top)
-    //   $('#alberelli').css({
-    //     'position': 'fixed',
-    //     'top': 0,
-    //     'bottom': 'auto'
-    //   }); // restore when you scroll up
-  });
+$(document).scroll(function () {
+  if ($('#alberelli').offset().top + $('#alberelli').height()
+    >= $('footer').offset().top - 10) {
+    $('#alberelli').css('position', 'absolute');
+  }
+  if ($(document).scrollTop() + window.innerHeight < $('footer').offset().top) {
+    $('#alberelli').css('position', 'fixed'); // restore when you scroll up
+  }
+});
