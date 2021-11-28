24785c9d547034fc1c93496650acc52730c74b77
Paolo Porcellato <paolo.porcellato@gmail.com>
2021-11-28 09:59:31 +0100

    add overlay on menu bg, prevent scroll when menu is open

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,28 @@
 let btnMenuClose = document.getElementById("btn_menu_close");
 let btnMenuOpen = document.getElementById("btn_menu_open");
-btnMenuClose.addEventListener("click", () => {
+let overlay = document.getElementById("overlay");
+
+function closeMenu() {
   let menu = document.getElementById("menu");
+  let overlay = document.getElementById("overlay");
   menu.classList.add("menu--close");
   menu.classList.remove("menu--open");
+  overlay.classList.remove("active");
+  $('body').css('overflow', 'auto');
+}
+btnMenuClose.addEventListener("click", () => {
+  closeMenu();
 });
 btnMenuOpen.addEventListener("click", () => {
   let menu = document.getElementById("menu");
+  let overlay = document.getElementById("overlay");
   menu.classList.add("menu--open");
   menu.classList.remove("menu--close");
+  overlay.classList.add("active");
+  $('body').css('overflow', 'hidden');
+});
+overlay.addEventListener("click",() => {
+  closeMenu();
 });
 
 $(document).scroll(function () {
