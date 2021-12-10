50f1b8c5d31d6a43a750dc64c7e70e9857e1de44
Paolo Porcellato <paolo.porcellato@gmail.com>
2021-12-10 10:59:11 +0100

    Co-authored-by: annachiara97 <annachiara97@users.noreply.github.com>

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,11 +1,36 @@
-const swiper = new Swiper('.swiper', {
+const swiperHouses = new Swiper('.swiper1', {
     // Optional parameters
     loop: true,
-    effect: 'cards',
+    // effect: 'cards',
+    // If we need pagination
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination1',
+    },
+    // paginationClickable: true,
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  const swiperExperiences = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    // effect: 'cards',
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination2',
     },
+    // paginationClickable: true,
   
     // Navigation arrows
     // navigation: {
